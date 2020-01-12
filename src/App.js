c127d71d3071d2f71ca4f68469dbbaffc9c9c1ee
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-01-12 18:26:17 -0500

    Added create quiz validation and implemented quiz storage into firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import { Context, initialState, reducer } from "./store";
-import { Container, Row, Col, Navbar, Button } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Button, Form } from "react-bootstrap";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
@@ -10,22 +10,32 @@ import { createQuiz } from "./components/quiz/quizHelpers";
 
 function App() {
   const [store, dispatch] = useReducer(reducer, initialState);
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [validated, setValidated] = useState(false);
   let history = useHistory();
   // determine if user is creating or taking a test
   const create = useRouteMatch("/create/:quizID");
 
-  console.log("Store:\n", store);
+  // console.log("Store:\n", store);
 
-  const handleCreate = () => {
-    const quizID = createQuiz();
-    history.push(`/create/${quizID}`);
+  const handleSubmit = async event => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    setValidated(true);
+
+    if (form.checkValidity() === true) {
+      const quizID = await createQuiz({ userName, userEmail });
+      history.push(`/create/${quizID}`);
+    }
   };
 
   return (
     <Context.Provider value={{ store, dispatch }}>
       <div className="App">
         <Container>
-          <Navbar bg="dark" variant="dark">
+          <Navbar bg="dark" variant="dark" className="mb-4">
             <Navbar.Brand href="/">
               <img
                 alt=""
@@ -39,9 +49,49 @@ function App() {
           </Navbar>
           {!create && (
             <Row>
-              <Button variant="outline-success" onClick={() => handleCreate()}>
-                Create Quiz
-              </Button>
+              <Col>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Row>
+                    <Col>
+                      <Form.Group controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          required
+                          type="text"
+                          placeholder="Name"
+                          value={userName}
+                          onChange={e => setUserName(e.target.value)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Name is required
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          required
+                          type="email"
+                          placeholder="Enter email"
+                          value={userEmail}
+                          onChange={e => setUserEmail(e.target.value)}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Valid email is required
+                        </Form.Control.Feedback>
+                        <Form.Text className="text-muted">
+                          We'll never share your email with anyone else.
+                        </Form.Text>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+
+                  <Button type="submit" variant="outline-success">
+                    Create Quiz
+                  </Button>
+                </Form>
+              </Col>
             </Row>
           )}
           {create && (
--- src/components/quiz/quizHelpers.js
+++ src/components/quiz/quizHelpers.js
@@ -14,13 +14,17 @@ export const submitQuiz = creatorAnswers => {
   return "submitted";
 };
 
-export const createQuiz = () => {
+export const createQuiz = async ({ userName, userEmail }) => {
   const db = firebase.firestore();
   const quizHash = Math.floor(Math.random() * Math.floor(1000000000));
+  const userData = { userName, userEmail };
+  const user = await db.collection("users").add(userData);
+  const quizData = { quizID: quizHash };
 
-  db.collection("quizzes").add({
-    quizID: quizHash
-  });
+  db.collection("users")
+    .doc(user.id)
+    .collection("quizzes")
+    .add(quizData);
 
   return quizHash;
 };
