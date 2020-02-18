4a90f36b4efd6af93b887a5f1bd0594fb1369748
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-02-18 01:39:23 -0500

    Added bottom margin to leaderboard

--- src/components/quiz/Leaderboard.js
+++ src/components/quiz/Leaderboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, Table } from "react-bootstrap";
 
 export const Leaderboard = ({ data }) => (
-  <Card>
+  <Card className="mb-4">
     <Card.Header className="text-center">
       <h3>Leaderboard</h3>
     </Card.Header>
