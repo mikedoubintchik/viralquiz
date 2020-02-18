2031b766bd8f04e166e5faf0815e1be8df10a8ac
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-02-18 00:52:24 -0500

    Updated Display Share layout

--- src/components/DisplayShare.js
+++ src/components/DisplayShare.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import {
   EmailShareButton,
   EmailIcon,
@@ -12,40 +12,45 @@ import {
 } from "react-share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const DisplayShare = props => {
-  const url = `${window.location.origin}/${props.quizID}`;
+const DisplayShare = ({ quizID, quizName }) => {
+  const url = `${window.location.origin}/${quizID}`;
 
   return (
-    <div className="share d-flex mb-4 justify-content-between">
-      <FacebookShareButton url={url} quote={props.quizName} className="p-2">
-        <FacebookIcon size={32} round />
-      </FacebookShareButton>
-      <WhatsappShareButton
-        url={url}
-        title={props.quizName}
-        separator=":: "
-        className="p-2"
-      >
-        <WhatsappIcon size={32} round />
-      </WhatsappShareButton>
-      <TwitterShareButton url={url} title={props.quizName} className="p-2">
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-      <EmailShareButton
-        url={url}
-        subject={props.quizName}
-        body="Take my quiz!"
-        className="p-2"
-      >
-        <EmailIcon size={32} round />
-      </EmailShareButton>
+    <Card className="mb-4">
+      <Card.Header className="text-center">Share Quiz</Card.Header>
+      <Card.Body>
+        <div className="share d-flex flex-wrap justify-content-between">
+          <FacebookShareButton url={url} quote={quizName} className="p-2">
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <WhatsappShareButton
+            url={url}
+            title={quizName}
+            separator=":: "
+            className="p-2"
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+          <TwitterShareButton url={url} title={quizName} className="p-2">
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <EmailShareButton
+            url={url}
+            subject={quizName}
+            body="Take my quiz!"
+            className="p-2"
+          >
+            <EmailIcon size={32} round />
+          </EmailShareButton>
 
-      <CopyToClipboard text={url}>
-        <Button variant="primary" size="sm">
-          Copy Quiz URL
-        </Button>
-      </CopyToClipboard>
-    </div>
+          <CopyToClipboard text={url}>
+            <Button variant="primary" size="sm">
+              Copy Quiz URL
+            </Button>
+          </CopyToClipboard>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
