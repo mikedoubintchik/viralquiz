bec2afdc776dff02458c36abe063bbb1cd582f54
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-03-29 00:37:54 -0400

    Added font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useReducer } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
-import { Context, initialState, reducer } from "./store";
 import { Container, Row, Col, Navbar, Button } from "react-bootstrap";
-import logo from "./logo.svg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+// library css
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+// custom css and images
 import "./custom.scss";
+import logo from "./logo.svg";
+
+// global store
+import { Context, initialState, reducer } from "./store";
+
+// components
 import RegisterUser from "./components/RegisterUser";
 import DisplayQuiz from "./components/quiz/DisplayQuiz";
 import QuizCreated from "./components/QuizCreated";
 import DisplayQuizResults from "./components/DisplayQuizResults";
+
+library.add(fab);
 
 const App = () => {
   const [store, dispatch] = useReducer(reducer, initialState);
